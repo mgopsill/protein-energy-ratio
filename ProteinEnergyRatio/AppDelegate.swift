ea8e729f0968a7655e9d6d93fa1550e1d836e13a
Mike Gopsill <m.gopsill@gmail.com>
2019-09-11 09:04:29 +0100

    Basic Autocomplete Functionality Working

--- ProteinEnergyRatio/AppDelegate.swift
+++ ProteinEnergyRatio/AppDelegate.swift
@@ -16,7 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let foodSearchViewController = FoodSearchViewController()
+        let navigationController = UINavigationController(rootViewController: foodSearchViewController)
+        
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
+        return true        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
