9146ddb817e10e0e64ce98a0e171abc5996e3e67
Mike Gopsill <m.gopsill@gmail.com>
2019-10-28 16:30:57 +0000

    feat: more labels and logic

--- ProteinEnergyRatio-iOS/MainViewController.swift
+++ ProteinEnergyRatio-iOS/MainViewController.swift
@@ -21,6 +21,9 @@ public class MainViewController: UIViewController {
     
     private let caloriesLabel = UILabel()
     private let percentProteinLabel = UILabel()
+    private let percentFatLabel = UILabel()
+    private let percentCarbLabel = UILabel()
+    private let proteinEnergyRatioLabel = UILabel()
 
     private let bag = DisposeBag()
     
@@ -66,22 +69,53 @@ public class MainViewController: UIViewController {
             make.top.equalTo(fiberSlider.snp.bottom)
             make.leading.trailing.equalToSuperview()
         }
+        
         view.addSubview(percentProteinLabel)
         percentProteinLabel.backgroundColor = .purple
         percentProteinLabel.snp.makeConstraints { make in
             make.top.equalTo(caloriesLabel.snp.bottom)
             make.leading.trailing.equalToSuperview()
         }
-
+        
+        view.addSubview(percentFatLabel)
+        percentFatLabel.backgroundColor = .green
+        percentFatLabel.snp.makeConstraints { make in
+            make.top.equalTo(percentProteinLabel.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        view.addSubview(percentCarbLabel)
+        percentCarbLabel.backgroundColor = .orange
+        percentCarbLabel.snp.makeConstraints { make in
+            make.top.equalTo(percentFatLabel.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        view.addSubview(proteinEnergyRatioLabel)
+        proteinEnergyRatioLabel.backgroundColor = .cyan
+        proteinEnergyRatioLabel.snp.makeConstraints { make in
+            make.top.equalTo(percentCarbLabel.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+        }
+        
     }
     
     func bindUI() {
-        let (caloriesLabelValue, percentProteinLabelValue) = mainViewModel(proteinSliderInput: proteinSlider.value.asObservable(),
-                                                                           fatSliderInput: fatSlider.value.asObservable(),
-                                                                           carbohydrateSliderInput: carbohydrateSlider.value.asObservable(),
-                                                                           fiberSliderInput: fiberSlider.value.asObservable())
-        caloriesLabelValue.drive(caloriesLabel.rx.text).disposed(by: bag)
-        percentProteinLabelValue.drive(percentProteinLabel.rx.text).disposed(by: bag)
+        let (caloriesLabelValue,
+            percentProteinLabelValue,
+            percentFatLabelValue,
+            percentCarbLabelValue,
+            proteinRatioValue) =
+            mainViewModel(proteinSliderInput: proteinSlider.value.asObservable(),
+                          fatSliderInput: fatSlider.value.asObservable(),
+                          carbohydrateSliderInput: carbohydrateSlider.value.asObservable(),
+                          fiberSliderInput: fiberSlider.value.asObservable())
+        caloriesLabelValue.map { "Cals: \(String($0)) "}.drive(caloriesLabel.rx.text).disposed(by: bag)
+        percentProteinLabelValue.map { "Protein percent: \(String($0)) "}.drive(percentProteinLabel.rx.text).disposed(by: bag)
+        percentFatLabelValue.map { "Fat percent: \(String($0)) "}.drive(percentFatLabel.rx.text).disposed(by: bag)
+        percentCarbLabelValue.map { "Carb percent: \(String($0)) "}.drive(percentCarbLabel.rx.text).disposed(by: bag)
+        proteinRatioValue.map { "P:E \(String($0)) "}.drive(proteinEnergyRatioLabel.rx.text).disposed(by: bag)
+
     }
 }
 
@@ -91,9 +125,11 @@ func mainViewModel(
     carbohydrateSliderInput: Observable<Float>,
     fiberSliderInput: Observable<Float>
     ) -> (
-    Driver<String>,
-    Driver<String>
-
+    Driver<Int>,
+    Driver<Int>,
+    Driver<Int>,
+    Driver<Int>,
+    Driver<Float>
     ) {
         
         func proteinAsCalories(_ float: Float) -> Int {
@@ -108,20 +144,47 @@ func mainViewModel(
             return Int(a - b) * 4
         }
         
-        func sum(_ a: Int, _ b: Int, _ c: Int) -> String {
-            return "Calories: \(a + b + c)"
+        func sum(_ a: Int, _ b: Int, _ c: Int) -> Int {
+            return a + b + c
         }
         
-        let proteinCalories = proteinSliderInput.map(proteinAsCalories).debug("proteinCals")
-        let fatCalories = fatSliderInput.map(fatAsCalories).debug("fatCals")
-        let carbohydrateCalories = Observable.combineLatest(carbohydrateSliderInput, fiberSliderInput).map(carbsMinusFiber).debug("carbcals")
+        func percent(_ a: Float, _ b: Float) -> Int {
+            guard a > 0 && b > 0 else { return 0 }
+            return Int((b / a) * 100)
+        }
         
-        let caloriesDriver = Observable.combineLatest(proteinCalories, fatCalories, carbohydrateCalories).map(sum).asDriver(onErrorJustReturn: "")
+        func ratio(_ protein: Float, _ fat: Float, _ carbohydrates: Float, _ fiber: Float) -> Float {
+            let ratio = protein / (fat + carbohydrates - fiber)
+            let returnValue = (ratio * 10).rounded(.toNearestOrEven) / 10
+            return returnValue > 0 ? returnValue : 0
+        }
         
-            
+        let proteinCalories = proteinSliderInput.map(proteinAsCalories).debug("proteincals")
+        let fatCalories = fatSliderInput.map(fatAsCalories)
+        let carbohydrateCalories = Observable.combineLatest(carbohydrateSliderInput, fiberSliderInput).map(carbsMinusFiber)
+        
+        let caloriesDriver = Observable.combineLatest(proteinCalories, fatCalories, carbohydrateCalories).map(sum).asDriver(onErrorJustReturn: 0)
+        let percentProteinDriver = Observable.combineLatest(caloriesDriver.asObservable().map(Float.init), proteinCalories.map(Float.init)).map(percent).asDriver(onErrorJustReturn: 0)
+        let percentFatDriver = Observable.combineLatest(caloriesDriver.asObservable().map(Float.init), fatCalories.map(Float.init)).map(percent).asDriver(onErrorJustReturn: 0)
+        let percentCarbDriver = Observable.combineLatest(caloriesDriver.asObservable().map(Float.init), carbohydrateCalories.map(Float.init)).map(percent).asDriver(onErrorJustReturn: 0)
+        let proteinRatioDriver = Observable.combineLatest(proteinSliderInput, fatSliderInput, carbohydrateSliderInput, fiberSliderInput).map(ratio).asDriver(onErrorJustReturn: 0)
+        
+        
+//        var ratio = fixNum(protein / (fat + carbohydrates - fiber));
+//
+//        ratio = Math.round(ratio * 100) / 100;
+//
+//        var nutritionalVector = Math.atan(ratio)*180/Math.PI;
+//
+//        nutritionalVector = Math.round(nutritionalVector * 10) / 10;
 
+        // TODO: NVector
+        // TODO: Fiber vs carb slider logic
         return (
             caloriesDriver,
-            Driver<String>.just("Test 2")
+            percentProteinDriver,
+            percentFatDriver,
+            percentCarbDriver,
+            proteinRatioDriver
         )
 }
